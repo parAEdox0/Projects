3d66c192275fdc6205bfa795cd248d58c4986639
Parth Ekbote <paraedox0@gmail.com>
2024-02-14 14:43:14 +0530

    Update script.js

--- Gsap/script.js
+++ Gsap/script.js
@@ -19,7 +19,7 @@ tl.from(".leftMain", {
     duration: 0.3
 })
 
-tl.from("h1, .headingImages, h4", {
+tl.from(".page1 h1, .headingImages, h4", {
     x: -200,
     opacity: 0,
     duration: 0.8
@@ -34,4 +34,28 @@ tl.from("#rightImage, .dots", {
 tl.from(".rt", {
     opacity: 0,
     duration: 0.5
+})
+
+gsap.from(".page2", {
+    opacity: 0,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".page2",
+        scroller: "body",
+        start: "top 40%",
+        end: "top 10%",
+        scrub: 2,
+    }
+})
+
+gsap.from(".twoHeading", {
+    opacity: 0,
+    scale: 0.5,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".twoHeading",
+        scroller: "body",
+        end: "top 10%",
+        scrub: 2,
+    }
 })
