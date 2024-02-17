a92a4b4035144a4463dced5f6008625c7d0a1709
Parth Ekbote <paraedox0@gmail.com>
2024-02-17 23:06:54 +0530

    Update script.js

--- Golf/script.js
+++ Golf/script.js
@@ -1,8 +1,37 @@
 var tl = gsap.timeline();
 
-tl.from(".nav", {
+gsap.to(".nav", {
     backgroundColor: "black",
     height: "16vh",
-    scrollTrigger()
+    scrollTrigger: {
+        trigger: ".nav",
+        scroller: "body",
+        start: "top -2",
+        scrub: 1
+    }
+})
+
+gsap.to(".vidCover",  {
+    backgroundColor: "black",
+    scrollTrigger: {
+        trigger: ".vidCover",
+        scroller: "body",
+        start: "top -20%",
+        scrub: 1
+    }
+})
+
+gsap.to(".banner", {
+    marginTop: "-250px",
+    duration: 4,
+    opacity: "1",
+    scrollTrigger: {
+        trigger: ".banner",
+        scroller: "body",
+        // markers: true,
+        start: "top 120%%",
+        end: "top 125%",
+        scrub: 4
+    }
 })
 
